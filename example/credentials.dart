a863e92bf04b1310d7a023757feb09d3a06a5e08
Debora Amaral <debora.amaral@sejaefi.com.br>
2025-09-16 14:36:10 -0300

    Updates

--- example/credentials.dart
+++ example/credentials.dart
@@ -1,6 +1,6 @@
 Map<String, dynamic> credentials = {
-  'client_id': 'Client_Id_924c64f85bf2615285f5bb9d06b0619f2e99bc15',
-  'client_secret': 'Client_Secret_a0ffd9c540b31dc286f00f07f1074fcfa8ad07d3',
+  'client_id': '',
+  'client_secret': '',
   'sandbox': false,
-  'certificate': '/home/vagrant/sdk-dart-apis-efi/example/producao-615625-consultoria.p12',
+  'certificate': '/certificado.p12',
 };
